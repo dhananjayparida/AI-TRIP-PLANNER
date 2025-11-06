f09a4b4ecde477d1ea3192736d600f1361b216d8
dhananjayparida <dhananjayparida149@gmail.com>
2025-11-07 00:41:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ app = Flask(__name__)
 def home(): 
     return "hello"
 
-# @app.route('/Smart_Trip_Planner/<string:source>/<string:destination>/<int:days>/<int:budget>', methods=['GET'])
-# def smart_trip_planner(source , destination, days, budget):
-#     detail=at.generate_trip_plan(source, destination, days, budget).replace("\n", "<br>")
-#     return jsonify(detail)
+@app.route('/Smart_Trip_Planner/<string:source>/<string:destination>/<int:days>/<int:budget>', methods=['GET'])
+def smart_trip_planner(source , destination, days, budget):
+    detail=at.generate_trip_plan(source, destination, days, budget).replace("\n", "<br>")
+    return jsonify(detail)
 
-if(__name__=="__main__"):
-    app.run(host='0.0.0.0',debug=True)
-
-
-
-
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
